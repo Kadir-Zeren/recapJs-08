bb612ec64c0e09b7a577e08e36e6584d6fc0dc6b
kadir-zeren <k4dzeren@gmail.com>
2024-04-19 20:15:22 +0300

    indexing

---
+++ array.js
@@ -0,0 +1,49 @@
+// const arr = [100, "CW", false];
+// console.log(arr);
+
+// const dizi = new Array(100, "CW", true);
+// console.log(dizi);
+
+// const dizi2 = new Array(10);
+// console.log(dizi2);
+
+// const colors = ["Red", "Green", "Blue"];
+
+// console.log(colors[0]);
+// console.log(colors[colors.length - 1]);
+// console.log(colors[colors.length - 2]);
+// console.log(colors[-1]);
+// console.log(colors[56]);
+
+// const colors = ["Red", "Green", "Blue"];
+// console.log(typeof colors);
+
+// console.log(Array.isArray(colors));
+// console.log(colors instanceof Array);
+
+// const variables = "1";
+// console.log(Array.isArray(variables));
+// console.log(variables instanceof Array);
+
+// let str = "Brown";
+// console.log(str[0]);
+// str[0] = "b";
+// console.log(str);
+
+// str = "brown";
+
+const colors = ["Red", "Green", "Blue"];
+
+colors[1] = "yellow";
+
+console.log(colors);
+
+colors[3] = "White";
+
+console.log(colors);
+
+let c = "Pink";
+
+colors[4] = c;
+
+console.log(colors);
