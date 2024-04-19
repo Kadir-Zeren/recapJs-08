6830c937e58133c50ce971aaca607ed84f5fc9e4
kadir-zeren <k4dzeren@gmail.com>
2024-04-19 20:45:13 +0300

    push

--- array.js
+++ array.js
@@ -32,18 +32,103 @@
 
 // str = "brown";
 
-const colors = ["Red", "Green", "Blue"];
+// const colors = ["Red", "Green", "Blue"];
 
-colors[1] = "yellow";
+// colors[1] = "yellow";
 
-console.log(colors);
+// console.log(colors);
 
-colors[3] = "White";
+// colors[3] = "White";
 
-console.log(colors);
+// console.log(colors);
 
-let c = "Pink";
+// let c = "Pink";
 
-colors[4] = c;
+// colors[4] = c;
 
-console.log(colors);
+// console.log(colors);
+
+// const colors = ["Red", "Green", "Blue"];
+
+// colors[6] = 2;
+// console.log(colors);
+// console.log(colors.length);
+// console.log(colors[3]);
+
+// colors[100] = "Pink";
+
+// console.log(colors);
+
+// let a = 5;
+// let b = 5;
+// console.log(a);
+// console.log(b);
+
+// a = 6;
+// console.log(b);
+
+// b = a;
+// console.log(b);
+
+// let arr1 = [1, 2, "Three", false];
+
+// const arr2 = arr1;
+
+// console.log(arr1);
+// console.log(arr2);
+
+// arr1[0] = 0;
+
+// console.log(arr1);
+// console.log(arr2);
+
+// const c = ["2"];
+// console.log(c);
+
+// console.log(Boolean(c));
+
+// const d = ["2"];
+// console.log(c == d);
+
+// console.log(arr1 == arr2);
+
+// const colors = ["Red", "Green", "Blue", "Yellow", "Brown"];
+
+// const cls = colors.slice();
+
+// console.log(colors);
+// console.log(cls);
+
+// colors[0] = "Black";
+
+// console.log(colors);
+// console.log(cls);
+
+// console.log(colors.slice(2, 4));
+// console.log(colors.slice(-2));
+// console.log(colors.slice(2));
+// console.log(colors.slice(8));
+// console.log(colors.slice(-2, 1));
+// console.log(colors.slice(-3, -1));
+
+const colors = ["Red", "Green"];
+console.log(colors.length);
+
+let c = "White";
+
+// const d = colors.push("Blue", 2023, c);
+
+console.log(colors.push("Blue", 2023, c));
+// colors.push(2023);
+
+// console.log(colors.length);
+// console.log(d + " d");
+
+const car = ["Audi", "BMW"];
+
+const car2 = ["Ford", "Tesla", "Mercedes"];
+
+car.push(car2);
+car.push(...car2);
+
+console.log(car[2][2]); //Mercedes
